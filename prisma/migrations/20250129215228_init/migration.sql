5e3cea36646dc79b9d5e8e57c8bc4b6a2e7fd2f1
NICOLE CABRERA <nicolecabreralopez@gmail.com>
2025-01-29 15:54:39 -0600

    step number 2. Build the skeleton of the backedn using npm and add the dependencies

---
+++ prisma/migrations/20250129215228_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_firstName_key" ON "User"("firstName");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_lastName_key" ON "User"("lastName");
